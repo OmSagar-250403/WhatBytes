a9bec539113a5682dd390350776f39fc15d506f3
Om <omsagar250403@gmail.com>
2025-02-14 13:20:10 +0530

    Updated Email html

--- project/app/views.py
+++ project/app/views.py
@@ -63,6 +63,7 @@ def register(request):
 def activate(request, uidb64, token):
     try:
         uid = urlsafe_base64_decode(uidb64).decode()
+        print(f'uid: {uid}')
         user = User.objects.get(pk=uid)
     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
